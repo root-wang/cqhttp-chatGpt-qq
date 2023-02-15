f2afeb834790930274c31b1fe82d1d2f3fe311ca
root_wang <276211640@qq.com>
2023-02-15 18:27:21 +0800

    log更新

--- src/log/file/file.go
+++ src/log/file/file.go
@@ -12,7 +12,7 @@ import (
 )
 
 func InitLogFile() io.Writer {
-	file := path.Join(time.Now().Format("2006y01m02d15h04m") + ".log")
+	file := path.Join(time.Now().Format("2006y01m02d15h") + ".log")
 	logFile, err := os.OpenFile(file, os.O_CREATE|os.O_APPEND|os.O_RDWR, os.ModePerm)
 	if err != nil {
 		panic(err)
